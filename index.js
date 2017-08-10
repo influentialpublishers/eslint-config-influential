316d1711bbbeba80e2a08a8ee34a5c3c991cb9f4
Asa Ayers <aayers@influential.co>
2017-08-10 14:53:39 -0700

    Update eslint config to match our current standards.

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
+// @noflow
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "plugin:flowtype/recommended",
-    "standard",
-    "plugin:unicorn/recommended",
+  'parser': 'babel-eslint',
+  'extends': [
+    'plugin:flowtype/recommended',
+    'standard',
+    'plugin:unicorn/recommended',
   ],
-  "plugins": [
-    "flowtype",
-    "standard",
-    "promise",
-    "unicorn",
+  'plugins': [
+    'flowtype',
+    'standard',
+    'promise',
+    'unicorn',
   ],
-  "rules": {
-    "comma-style": [ "error", "last" ],
-    "comma-angle": [ "error", "always-multiline" ],
-    "prefer-const": [ "error" ],
-    "no-var": [ "error" ],
-    "camelcase": 0,
-    // We put exports first
-    "import/first": 0,
-    "no-use-before-define": 0,
-    "flowtype/type-id-match": [
+  'rules': {
+    'arrow-parens': ['error', 'always'],
+    'camelcase': 0,
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'flowtype/require-valid-file-annotation': [
+      2,
+      'always',
+      { 'annotationStyle': 'line' },
+    ],
+    'flowtype/type-id-match': [
       2,
       // Only enforce that it starts with a capital letter
-      "^[A-Z][a-z0-9]*"
+      '^[A-Z][a-z0-9]*',
     ],
-    "flowtype/require-valid-file-annotation": [
-      2,
-      "always",
-      { "annotationStyle": "line" }
-    ]
-  }
-};
+    'import/extensions': [ 'error', 'always', { 'js': 'never' } ],
+    'import/newline-after-import': [ 'error' ],
+    'import/no-internal-modules': [ 'error', {
+      'allow': [ '**/lib/*' ],
+    } ],
+    'import/no-namespace': 'error',
+    'no-await-in-loop': [ 'error' ],
+    'no-confusing-arrow': ['error', {'allowParens': true}],
+    'no-console': [ 'error' ],
+    'no-implicit-coercion': [ 'error' ],
+    'no-unused-vars': ['error', {
+      'vars': 'all',
+      'args': 'all',
+      'caughtErrors': 'all',
+      'varsIgnorePattern': '[iI]gnored',
+      'argsIgnorePattern': '[iI]gnored',
+      'caughtErrorsIgnorePattern': '[iI]gnored',
+      'ignoreRestSiblings': true,
+    }],
+    'no-var': [ 'error' ],
+    'prefer-const': [ 'error' ],
+    'prefer-template': [ 'error' ],
+    'require-await': [ 'error' ],
+  },
+}
